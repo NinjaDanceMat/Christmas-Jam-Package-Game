05b08d618329786769be0d5ee535cb3b5d87e6c4
NinjaDanceMat <cathcart100@googlemail.com>
2023-12-21 16:13:50 +0000

    Enemy gets hurt

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -9,22 +9,53 @@ public class EnemyScript : MonoBehaviour
 
     public bool goingTowardsStart;
     public float speed;
+    public bool dead;
+
+    public int currentHealth;
+    public int maxHealth;
+    public Animator animationController;
+    public void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 destination;
-        if (goingTowardsStart)
+        if (!dead)
         {
-            destination = StartPoint.position;
-        }
-        else
-        {
-            destination = EndPoint.position;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
-        if (Vector3.Distance(transform.position,destination)< 0.1f)
-        {
-            goingTowardsStart = !goingTowardsStart;
+            Vector3 destination;
+            if (goingTowardsStart)
+            {
+                destination = StartPoint.position;
+            }
+            else
+            {
+                destination = EndPoint.position;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+            if (Vector3.Distance(transform.position, destination) < 0.1f)
+            {
+                goingTowardsStart = !goingTowardsStart;
+            }
         }
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!dead)
+        {
+            if (collision.gameObject.tag == "PlayerProjectile")
+            {
+                animationController.SetTrigger("Hit");
+                currentHealth -= 1;
+                if (currentHealth <= 0)
+                {
+                    dead = true;
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+
 }
